c21e0eec533aa113a33cfd2ddddda8b5dea95cce
William Renfrew <william@synergitech.co.uk>
2019-07-04 14:25:23 +0100

    Remove database name lookup

--- classes/migration.php
+++ classes/migration.php
@@ -14,33 +14,13 @@ class Migration
      */
     public static function check_index_exists($table_name, $index_name)
     {
-        $dsn = \Config::get('db.'.\Config::get('db.active').'.connection.dsn');
-        foreach (explode(';', $dsn) as $dsn_part) {
-            $exploded = explode('=', $dsn_part);
-            if ($exploded[0] == 'dbname') {
-                $database_name = $exploded[1];
-                break;
-            }
-        }
+        $query = \DB::select('*')
+            ->from('information_schema.statistics')
+            ->where('table_schema', \DB::expr('DATABASE()'))
+            ->where('table_name', $table_name)
+            ->where('index_name', $index_name)
+            ->execute();
 
-        if (!isset($database_name)) {
-            throw new Exception('Database Name not able to fetched from DSN string');
-        }
-
-        try {
-            $query = \DB::select('*')
-                ->from('information_schema.statistics')
-                ->where('table_schema', $database_name)
-                ->where('table_name', $table_name)
-                ->where('index_name', $index_name)
-                ->execute();
-
-            if ($query->count() > 0) {
-                return true;
-            }
-        } catch (\Exception $e) {
-            return false;
-        }
-        return false;
+        return $query->count() > 0;
     }
 }
